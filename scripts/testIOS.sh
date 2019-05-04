5a7b7c81169d34144b3207ccd458dca689e7765d
ph1p <phil@capshake.com>
2019-05-04 21:16:33 +0200

    Update react-navigation to 2.x.x and react-native to 0.57.5
    Update build testIOS.sh
    Update flow version
    Update travis file to newest node LTS and update iphones

--- scripts/testIOS.sh
+++ scripts/testIOS.sh
@@ -8,4 +8,4 @@ SIMVER=$2
 echo Testing ${SIMNAME} with iOS ${SIMVER}...
 
 cd $THISDIR/../ios
-set -o pipefail && xcodebuild -scheme Buttercup -destination "platform=iOS Simulator,name=${SIMNAME},OS=${SIMVER}" build test | xcpretty -c
+set -o pipefail && xcodebuild -UseModernBuildSystem=NO -scheme Buttercup -destination "platform=iOS Simulator,name=${SIMNAME},OS=${SIMVER}" build test | xcpretty -c
